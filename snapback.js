e4959117ee5c94bb49e8374433434e5aa64dc796
Linus Miller <lohfu@lohfu.io>
2014-08-02 15:24:00 +0200

    Dont know what i have done.

--- snapback.js
+++ snapback.js
@@ -1,8 +1,106 @@
-var Snapback = function(element) {
+var Snapback = function(element, config) {
 	var that = this;
-	function addMutation(mutation) {
+
+	this.config = typeof config === 'string' ? this.presets[config] : config;
+	this.element = element;
+	this.undos = [];
+	this.mutations = [];
+	this.undoIndex = -1;
+	this.active = false;
+	this.selection = null;
+
+	this.timeout = null;
+	this.isTyping = false;
+	this.oldValue = null;
+	this.newValue = null;
+	this.observer = new MutationObserver(function(mutations) {
+		mutations.forEach(function(mutation) {
+			function finishedTyping() {
+				clearTimeout(timeout);
+				var undo = { target: target, oldValue: oldValue, newValue: newValue }; 
+				this.addMutation(undo);
+				setUndo();
+				oldValue = null;
+				newValue = null;
+				isTyping = false;
+			}
+			var fix = false;
+			switch(mutation.type) {
+				case 'childList':
+					//if(isTyping) finishedTyping();
+					if (mutation.target === element) {
+						_.each(mutation.addedNodes, function(node) {
+							if(node.nodeType === 3 || node.nodeName.toLowerCase() === 'div') {
+								fix = true;
+								var content = node.textContent !== '' ? node.textContent : '<br />';
+								var p = O('<p>' + content + '</p>');
+								that.toggle();
+								node.replaceWith(p);
+								that.toggle();
+								S.caret(p, false);
+								that.addMutation({ addedNodes: [ p ], removedNodes: [], target: mutation.target, next: p.nextSibling, prev: p.previousSibling });
+							}
+						});
+					} else {
+						_.each(mutation.addedNodes, function(node) {
+							if(node.nodeType ===1 ) {
+								node.removeAttribute('style');
+								if(node.nodeName.toLowerCase() === 'span') {
+									fix = true;
+									if(node.firstChild) {
+										var prev = node.previousSibling;
+										var oldValue = prev.textContent;
+										that.toggle();
+										prev.textContent = prev.textContent + node.textContent;
+										node.remove();
+										that.toggle();
+										that.addMutation({ target: prev, oldValue: oldValue, newValue: prev.textContent});
+										S.caret({ start: { node: prev, offset: oldValue.length }}, true);
+									} else {
+										node.remove();
+									}
+								}
+							}
+						});
+					}
+					if(!fix) that.addMutation(mutation);
+					break;
+				case 'characterData':
+					var undo = { target: mutation.target, oldValue: mutation.oldValue, newValue: mutation.target.textContent }; 
+					that.addMutation(undo);
+					//if(!isTyping) {
+					//	oldValue = mutation.oldValue;
+					//	isTyping = true;
+					//	target = mutation.target;
+					//} else {
+					//	clearTimeout(timeout);
+					//	newValue = mutation.target.textContent;
+					//}
+					//timeout = setTimeout(function() {
+					//	finishedTyping();
+					//}, 1000);
+					break;
+			}
+		});    
+	});
+
+};
+Snapback.prototype = {
+	presets: {
+		standard: {
+			mutationObserver: { subtree: true, childList: true },
+			selection: false,
+			type: false
+		},
+		spytext: {
+			mutationObserver: { subtree: true, childList: true, characterData: true, characterDataOldValue: true },
+			selection: true,
+			type: true
+		}
+	},
+	addMutation: function(mutation) {
 		if(mutation.oldValue) {
-			mutations.push({ target: mutation.target, oldValue: mutation.oldValue, newValue: mutation.target.textContent });
+			this.mutations.push({ target: mutation.target, oldValue: mutation.oldValue, newValue: mutation.target.textContent });
 		} else {
 			var addedNodes = [];
 			_.each(mutation.addedNodes, function(node) {
@@ -15,11 +113,50 @@ var Snapback = function(element) {
 				removedNodes.push({ target: node, next: next, prev: prev, parent: mutation.target });
 			});
 			var obj = { addedNodes: addedNodes, removedNodes: removedNodes};
-			mutations.push(obj);
+			this.mutations.push(obj);
 		}
-	}
-	function undoRedo(undo, isUndo) {
-		that.toggle();
+	},
+	isActive: function() {
+		return this.active;
+	},
+	redo: function() {
+		if(this.active && this.undoIndex < this.undos.length - 1) {
+			this.undoIndex++;
+			this.undoRedo(this.undos[this.undoIndex], false);
+		}
+	},
+	register: function() {
+		if(this.active && this.mutations.length > 0) {
+			if(this.undoIndex < this.undos.length - 1) {
+				this.undos = this.undos.slice(0, this.undoIndex + 1);
+			}
+			//undos.push({ selectionBefore: selectionBefore, selectionAfter: S.save(), mutations: mutations });
+			this.undos.push(this.mutations);
+			this.mutations = [];
+			this.undoIndex = this.undos.length -1;
+		}
+		//selectionBefore = S.save();
+	},
+	setSelection: function(selection) {
+		this.selection = selection ? selection : S.save();
+		console.log(this.selection);
+	},
+	size: function() {
+		return this.undos.length;
+	},
+	toggle: function() {
+		if(this.active) this.observer.disconnect();
+		else this.observer.observe(this.element, this.config.mutationObserver);
+		this.active = !this.active;
+	},
+	undo: function() {
+		if(this.active && this.undoIndex >= 0) {
+			this.undoRedo(this.undos[this.undoIndex], true);
+			this.undoIndex--;
+		}
+	},
+	undoRedo: function(undo, isUndo) {
+		this.toggle();
 		//var mutations = isUndo ? undo.mutations.slice(0).reverse() : undo.mutations;
 		var mutations = isUndo ? undo.slice(0).reverse() : undo;
 		for(var s = 0; s < mutations.length; s++) {
@@ -41,134 +178,6 @@ var Snapback = function(element) {
 				}
 			}
 		}
-		//if(isUndo) u.selectionBefore.load();
-		//else u.selectionAfter.load();
-		that.toggle();
+		this.toggle();
 	}
-	var config = { subtree: true, childList: true, characterData: true, characterDataOldValue: true };
-
-	var timeout = null;
-	var isTyping = false;
-	var oldValue = null;
-	var newValue = null;
-	var observer = new MutationObserver(function(mutations) {
-		mutations.forEach(function(mutation) {
-			function finishedTyping() {
-				clearTimeout(timeout);
-				var undo = { target: target, oldValue: oldValue, newValue: newValue }; 
-				addMutation(undo);
-				setUndo();
-				oldValue = null;
-				newValue = null;
-				isTyping = false;
-			}
-			var fix = false;
-			switch(mutation.type) {
-				case 'childList':
-					if(isTyping) finishedTyping();
-					if (mutation.target === element) {
-						_.each(mutation.addedNodes, function(node) {
-							if(node.nodeType === 3 || node.nodeName.toLowerCase() === 'div') {
-								fix = true;
-								var content = node.textContent !== '' ? node.textContent : '<br />';
-								var p = O('<p>' + content + '</p>');
-								that.toggle();
-								node.replaceWith(p);
-								that.toggle();
-								S.caret(p, false);
-								addMutation({ addedNodes: [ p ], removedNodes: [], target: mutation.target, next: p.nextSibling, prev: p.previousSibling });
-							}
-						});
-					} else {
-						_.each(mutation.addedNodes, function(node) {
-							if(node.nodeType ===1 ) {
-								node.removeAttribute('style');
-								if(node.nodeName.toLowerCase() === 'span') {
-									fix = true;
-									if(node.firstChild) {
-										var prev = node.previousSibling;
-										var oldValue = prev.textContent;
-										that.toggle();
-										prev.textContent = prev.textContent + node.textContent;
-										node.remove();
-										that.toggle();
-										addMutation({ target: prev, oldValue: oldValue, newValue: prev.textContent});
-										S.caret({ start: { node: prev, offset: oldValue.length }}, true);
-									} else {
-										node.remove();
-									}
-								}
-							}
-						});
-					}
-					if(!fix) addMutation(mutation);
-					break;
-				case 'characterData':
-					console.log('characterData');
-					var undo = { target: mutation.target, oldValue: mutation.oldValue, newValue: mutation.target.textContent }; 
-					addMutation(undo);
-					//if(!isTyping) {
-					//	oldValue = mutation.oldValue;
-					//	isTyping = true;
-					//	target = mutation.target;
-					//} else {
-					//	clearTimeout(timeout);
-					//	newValue = mutation.target.textContent;
-					//}
-					//timeout = setTimeout(function() {
-					//	finishedTyping();
-					//}, 1000);
-					break;
-			}
-		});    
-	});
-
-	var undoStack = [];
-	var mutations = [];
-	var undoIndex = -1;
-	var active = false;
-
-	this.size = function() {
-		return undoStack.length;
-	};
-	this.isActive = function() {
-		return active;
-	};
-	this.toggle = function() {
-		if(active) observer.disconnect();
-		else observer.observe(element, config);
-		active = !active;
-	};
-	this.register = function() {
-		console.log('adding to undoStack');
-		if(active && mutations.length > 0) {
-			if(undoIndex < undoStack.length - 1) {
-				console.log('clearing some undos');
-				undoStack = undoStack.slice(0, undoIndex + 1);
-			}
-			//undoStack.push({ selectionBefore: selectionBefore, selectionAfter: S.save(), mutations: mutations });
-			undoStack.push(mutations);
-			mutations = [];
-			undoIndex = undoStack.length -1;
-		}
-		//selectionBefore = S.save();
-	};
-	this.undo = function() {
-		if(active && undoIndex >= 0) {
-			undoRedo(undoStack[undoIndex], true);
-			undoIndex--;
-		} else {
-			console.log('undo: nothing to do');
-			console.log(undoIndex);
-		}
-	};
-	this.redo = function() {
-		if(active && undoIndex < undoStack.length - 1) {
-			undoIndex++;
-			undoRedo(undoStack[undoIndex], false);
-		} else {
-			console.log('redo: nothing to do');
-			console.log(undoIndex);
-		}
-	};
 };
