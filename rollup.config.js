82a8dbaff49e604090bc02491740aeb6948590bf
Linus Miller <lohfu@lohfu.io>
2018-05-06 20:27:57 +0200

    Update build process to use adjacent .js and .mjs files

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,14 @@
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel'
 
 export default {
   plugins: [
-    babel(),
-    nodeResolve({
-      jsnext: true,  // Default: false
-      main: true,  // Default: true
-      browser: true,  // Default: false
-    }), commonjs({
-      include: 'node_modules/**',  // Default: undefined
-      sourceMap: false,  // Default: true
-    })
+    babel({
+      exclude: 'node_modules/**/*',
+    }),
   ],
-  entry: 'src/snapback.js',
-  format: 'umd',
-  moduleName: 'Snapback'
-};
+  input: 'src/index.mjs',
+  output: {
+    format: 'umd',
+    name: 'snapback',
+  },
+}
